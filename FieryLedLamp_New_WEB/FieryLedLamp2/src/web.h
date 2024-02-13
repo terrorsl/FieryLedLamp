7f3daa85aa3fcf4794f18e04c5f80be913854824
terrorsl <terror85@gmail.com>
2024-02-13 18:27:01 +0300

    new

---
+++ FieryLedLamp_New_WEB/FieryLedLamp2/src/web.h
@@ -0,0 +1,16 @@
+#ifndef WEB_SERVER_FILE
+#define WEB_SERVER_FILE
+
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#elif defined(ESP32)
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#endif
+
+#include <ESPAsyncWebServer.h>
+#include "AsyncJson.h"
+#include "ArduinoJson.h"
+
+#endif
